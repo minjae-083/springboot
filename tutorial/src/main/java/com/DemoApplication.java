f9d564a6b0ddbc0679df0780d6ab1cf58bd35a11
min_jae083 <93437708+minjae-083@users.noreply.github.com>
2025-04-15 22:55:42 +0900

    Add files via upload

---
+++ tutorial/src/main/java/com/DemoApplication.java
@@ -0,0 +1,24 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class DemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("hello")
+	public String hello(Model model) {
+		model.addAttribute("nickname", "rhdrl");
+		return "templates-test1";
+	}
+}
