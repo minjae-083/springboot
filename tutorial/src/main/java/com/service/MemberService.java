6ec3eea8e62deb50a86e7918d516faa10470a6a1
min_jae083 <qaz066900@naver.com>
2025-05-27 13:18:40 +0000

    commit

--- tutorial/src/main/java/com/service/MemberService.java
+++ tutorial/src/main/java/com/service/MemberService.java
@@ -2,14 +2,16 @@ package com.service;
 
 import com.domain.Member;
 import com.repository.MemberRepository;
-import com.repository.MemoryMemberRepository;
 import java.util.List;
 import java.util.Optional;
 
 
 public class MemberService {
-  private final MemberRepository memberRepository = new MemoryMemberRepository();
+  private final MemberRepository memberRepository;
 
+  public MemberService(MemberRepository memberRepository){
+    this.memberRepository = memberRepository;
+  }
   public Long join(Member member) {
     // 중복 회원 검증 메서드 호출
     validateDuplicateMember(member);
--- tutorial/src/test/java/service/MemberServiceTest.java
+++ tutorial/src/test/java/service/MemberServiceTest.java
@@ -1,23 +1,42 @@
 package com.test.java.service;
 
 import com.domain.Member;
+import com.repository.MemoryMemberRepository;
 import com.service.MemberService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MemberServiceTest {
-  MemberService memberService = new MemberService();
+  MemoryMemberRepository memberRepository;
+  MemberService memberService;
   
   @Test
   void joinTest(){
+    // given
     Member member = new Member();
     member.setName("testMember");
 
+    // when
+    // 회원가입, 반환 id를 saveId에 저장
     Long saveId = memberService.join(member);
 
+    // then
+    // 회원가입한 member의 id로 회원 조회, 해당 객체 findMember에 저장
     Member findMember = memberService.findOne(saveId).get();
-
+    // 객체 동일성 비교
     assertThat(member.getName()).isEqualTo(findMember.getName());
+  }
+
+  @BeforeEach
+  public void beforeEach(){
+    memberRepository = new MemoryMemberRepository();
+    memberService = new MemberService(memberRepository);
+  }
+  @AfterEach
+  public void afterEach(){
+    memberRepository.clearStore();
   }
 }
