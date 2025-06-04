8185e9aada454a417cc6812f6304a9af3a7efa92
min_jae083 <qaz066900@naver.com>
2025-06-04 08:23:03 +0000

    cant find DataSource

--- tutorial/src/main/java/com/SpringConfig.java
+++ tutorial/src/main/java/com/SpringConfig.java
@@ -2,12 +2,20 @@ package com;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import com.repository.MemberRepository;
 import com.repository.MemoryMemberRepository;
 import com.service.MemberService;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 
 @Configuration
 public class SpringConfig {
+  private DataSource dataSource;
+  @Autowired
+  public SpringConfig(DataSource dataSource){
+    this.dataSource=dataSource;
+  }
+  
   @Bean
   public MemberService memberService(){
     // spring bean에 등록되어 있는 MemberRepository를 가져옴
@@ -16,6 +24,6 @@ public class SpringConfig {
 
   @Bean
   public MemberRepository memberRepository(){
-    return new MemoryMemberRepository();
+    return new JdbcMemberRepository(dataSource);
   }
 }
